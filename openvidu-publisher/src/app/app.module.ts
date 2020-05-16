8f40351558bd0449ba2aaa9c7e65cbce4a490ee3
Suvro Ghosh <suvro.j.ghosh@techl33t.in>
2020-05-16 21:45:22 +0530

    adding graphs and form

--- openvidu-publisher/src/app/app.module.ts
+++ openvidu-publisher/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { OpenviduSessionModule } from 'openvidu-angular';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent,
+
+    AnalyticsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    OpenviduSessionModule
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
